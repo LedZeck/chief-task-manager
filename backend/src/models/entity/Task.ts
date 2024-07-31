a86df1a8cb1db16bdaf67b2592aeeb43bee8c17b
Eric Araujo <zeckamtwo@gmail.com>
2024-07-31 08:46:24 -0300

    typeorm initial setup

---
+++ backend/src/models/entity/Task.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: false })
+  completed: boolean;
+}
